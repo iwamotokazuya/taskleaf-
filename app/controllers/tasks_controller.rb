252f3f12acd929bdbbbda41b5d147553bd607f7e
iwamotokazuya <kazuya10211026@gmail.com>
2021-09-12 23:06:00 +0900

    Add taskleaf

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit,:update, :destroy]
+
+  def index
+    @tasks = current_user.tasks.order(updated_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new 
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update!(task_params)
+    redirect_to task_url, notice: "タスク「#{@task.name}」を更新しました。"
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      logger.debug "task: #{@task.attributes.inspect}"
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def task_logger
+    @task_logger ||= Logger.new('log/task.log', 'daily')
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+end
